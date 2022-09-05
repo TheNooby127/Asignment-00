8cd7fb5b1132095d574b45d1cfce48b4e52b31e0
Daniel <danielmichael.fabricius@gmail.com>
2022-09-05 13:31:51 +0200

    Add project files.

---
+++ Asignment-00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Asignment_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
